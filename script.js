72a61d0e9a37b22d563e62a3fbc6e0165379da69
momocloud <phamhoangminh050899@gmail.com>
2021-03-03 16:31:31 +0700

    basic template for development

---
+++ script.js
@@ -0,0 +1,16 @@
+//main functions - add/subtract/multiply/divide
+function add (x, y){
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y){
+    return x * y;
+}
+
+function divide(x, y){
+    return x / y;
+}
